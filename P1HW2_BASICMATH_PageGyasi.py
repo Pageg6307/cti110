7c107c3ea837c9d6436c8dbbb2aa86c7d7ee2e59
Pageg6307 <107374767+Pageg6307@users.noreply.github.com>
2022-06-18 21:04:00 -0400

    Add files via upload

---
+++ P1HW2_BASICMATH_PageGyasi.py
@@ -0,0 +1,17 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+input()
+Cable
+'Cable'
+Num = input()
+110
+input()
+Monthly Charge
+'Monthly Charge'
+Num = input()
+110
+input()
+Annual Charge
+'Annual Charge'
+Num = input()
+1320
